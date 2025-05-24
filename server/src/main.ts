0c87a90e48638353b4933bb574ed59f5ba025ab6
tcrusel <crusel.thomas@gmail.com>
2025-05-24 14:54:55 +0200

    quest monorepo 02 finished

--- server/src/main.ts
+++ server/src/main.ts
@@ -12,6 +12,10 @@ import app from "./app";
 // Get the port from the environment variables
 const port = process.env.APP_PORT;
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Wild Series !");
+});
+
 // Start the server and listen on the specified port
 app
   .listen(port, () => {
