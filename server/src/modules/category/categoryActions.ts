bb85cef532c52bb43610b3d23cb2a0c96704b061
tcrusel <crusel.thomas@gmail.com>
2025-06-03 16:22:58 +0200

    Quest 06 repository done

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -1,3 +1,5 @@
+import categoryRepository from "./categoryRepository";
+
 // Some data to make the trick
 
 const categories = [
@@ -14,8 +16,10 @@ const categories = [
 // Declare the actions
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  res.json(categories);
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+
+  res.json(categoriesFromDB);
 };
 
 const read: RequestHandler = (req, res) => {
