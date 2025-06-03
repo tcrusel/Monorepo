4aaf59dc8b8ed62ff04e2cc3d62b0a3a494e179b
tcrusel <crusel.thomas@gmail.com>
2025-06-03 13:31:13 +0200

    Quest 05 BDD done

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -19,3 +19,54 @@ insert into item(id, title, user_id)
 values
   (1, "Stuff", 1),
   (2, "Doodads", 1);
+
+create table category (
+  id int primary key auto_increment,
+  name varchar(100)
+);
+
+create table program (
+  id int primary key auto_increment,
+  title varchar(50),
+  synopsis text not null,
+  poster varchar(255) not null,
+  country varchar(100) not null,
+  year int not null,
+  category_id int not null
+);
+
+insert into category(id, name)
+values
+  (1, "Comédie"),
+  (2, "Science-Fiction");
+
+insert into program
+  (
+    id,
+    title,
+    synopsis,
+    poster,
+    country,
+    year,
+    category_id
+  )
+values
+  (
+    1,
+    "The Good Place",
+    "À sa mort, Eleanor Shellstrop est envoyée au Bon Endroit, un paradis fantaisiste réservé aux individus exceptionnellement bienveillants. Or Eleanor n'est pas exactement une « bonne personne » et comprend vite qu'il y a eu erreur sur la personne. Avec l'aide de Chidi, sa prétendue âme sœur dans l'au-delà, la jeune femme est bien décidée à se redécouvrir.",
+    "https://img.betaseries.com/JwRqyGD3f9KvO_OlfIXHZUA3Ypw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2F94857341d71c795c69b9e5b23c4bf3e7.jpg",
+    "USA",
+    2016,
+    1
+  ),
+  (
+    2,
+    "Dark",
+    "Quatre familles affolées par la disparition d'un enfant cherchent des réponses et tombent sur un mystère impliquant trois générations qui finit de les déstabiliser.",
+    "https://img.betaseries.com/zDxfeFudy3HWjxa6J8QIED9iaVw=/600x900/smart/https%3A%2F%2Fpictures.betaseries.com%2Ffonds%2Fposter%2Fc47135385da176a87d0dd9177c5f6a41.jpg",
+    "Allemagne",
+    2017,
+    2
+  );
+  
